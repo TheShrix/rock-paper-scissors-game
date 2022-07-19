7021ac180123b74af3e9bafebdff0178d43ed17e
TheShrix <devyncloss@hotmail.com>
2022-07-19 16:35:12 -0400

    Refactored HTML and script files

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
-let computerScore = 0;
 let playerScore = 0;
+let computerScore = 0;
 
+const resultDiv = document.querySelector("#result");
+const playerScoreDiv = document.querySelector("#playerScore");
+const computerScoreDiv = document.querySelector("#computerScore");
+const WinningScore = 5;
+
+// function for computer's choice
 function computerPlay() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
   if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    console.log(
-      "You win! " + playerSelection + " beats " + computerSelection + "!"
-    );
+    resultDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
     playerScore++;
   } else if (playerSelection == computerSelection) {
-    console.log(`It is a tie. You both chose ${playerSelection}.`);
+    resultDiv.textContent = `Tis but a draw, you both chose ${playerSelection}`
   } else {
-    console.log(
-      "You lose! " +
-        playerSelection +
-        " does not beat " +
-        computerSelection +
-        "."
-    );
+    resultDiv.textContent = `You lose... ${computerSelection} beats ${playerSelection}.`;
     computerScore++;
   }
-}
 
-function game() {
-  computerScore = 0;
-  playerScore = 0;
+  playerScoreDiv.textContent = `Player Score: ${playerScore}`;
+  computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Choose rock, paper, or scissors!");
-    const computerSelection = computerPlay();
-
-    playRound(playerSelection, computerSelection);
-  }
-  if (playerScore > computerScore) {
-    console.log("You win!");
-  } else if (computerScore > playerScore) {
-    console.log("HAHAHAH YOU LOSE");
-  } else {
-    console.log("Tis but a draw.");
+  if (playerScore == WinningScore) {
+    resultDiv.textContent = "You Win!";
+    gameOver();
+  } else if (computerScore == WinningScore) {
+    resultDiv.textContent = "You Lose LMAO";
+    gameOver();
   }
 }
 
-game();
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button)=>{
+  button.addEventListener('click',clickButton)
+})
+
+function clickButton(e) {
+  let playerChoice = e.target.id;
+  let computerChoice = computerPlay();
+  playRound(playerChoice, computerChoice);
+}
+
+function gameOver() {
+  buttons.forEach((button)=>{
+    button.removeEventListener('click',clickButton);
+  })
+}
